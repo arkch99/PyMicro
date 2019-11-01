56c88149c8a0b7f73f081724eb61aa233da7e046
arkch99 <arko1999@gmail.com>
2019-11-01 21:22:18 +0530

    Added a working function to read in and store data

--- dataworks.py
+++ dataworks.py
@@ -4,9 +4,15 @@ import os
 fields = ("First name", "Middle name", "Last name", "DoB", "Gender", "Eating", "Hobby", "Phy", "Chem", "Maths", "Phone", "Mail", "Locality", "District", "City", "PO", "PS", "State", "Country", "PIN", "GName", "GRelation", "GOcc", "GPhone", "GMail")
 
 def newRecord(varDict): #varList is in the order maintained in new_entry
-    print(varDict)
+    #print(varDict)
+    firstFlag = False
     if not os.path.exists("records.csv"):
         open("records.csv", "x")
-    with open("records.csv", newline="") as database:
-        writer = csv.DictWriter(database, fields, restval = "")
+        firstFlag = True
+               
+    with open("records.csv", "a", newline="") as db:
+        if firstFlag:
+            headWriter = csv.writer(db)
+            headWriter.writerow(fields)
+        writer = csv.DictWriter(db, fields, restval = "")
         writer.writerow(varDict)
