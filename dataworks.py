a927b97c2f18707f25a6e292554b5dc08748ca0e
arkch99 <arko1999@gmail.com>
2019-11-05 23:40:46 +0530

    Calculate total and average

--- dataworks.py
+++ dataworks.py
@@ -1,10 +1,12 @@
 import csv
 import os
 
-fields = ("First name", "Middle name", "Last name", "DoB", "Gender", "Eating", "Hobby", "Phy", "Chem", "Maths", "Phone", "Mail", "Locality", "District", "City", "PO", "PS", "State", "Country", "PIN", "GName", "GRelation", "GOcc", "GPhone", "GMail")
+fields = ("First name", "Middle name", "Last name", "DoB", "Gender", "Eating", "Hobby", "Phy", "Chem", "Maths", "Total", "Average", "Phone", "Mail", "Locality", "District", "City", "PO", "PS", "State", "Country", "PIN", "GName", "GRelation", "GOcc", "GPhone", "GMail")
 
 def newRecord(varDict): #varList is in the order maintained in new_entry
     #print(varDict)
+    varDict["Total"] = float(varDict["Phy"]) + float(varDict["Chem"]) + float(varDict["Maths"])
+    varDict["Average"] = float(varDict["Total"]) / 3
     firstFlag = False
     if not os.path.exists("records.csv"):
         open("records.csv", "x")
