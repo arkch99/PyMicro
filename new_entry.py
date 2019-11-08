687318736c54ab8c4ef41bfbf0952792a88be839
arkch99 <arko1999@gmail.com>
2019-11-08 17:17:27 +0530

    Marked all mandatory fields

--- new_entry.py
+++ new_entry.py
@@ -185,13 +185,13 @@ def populate(container):
     Label(student,text="    Hobbies: ",font=sub).grid(row=5,sticky=W,pady=4)
     Entry(student, textvariable=l[6]).grid(row=5,column=1)
 
-    Label(student,text="    Physics marks: ",font=sub).grid(row=8,sticky=W,pady=4)
+    Label(student,text="    Physics marks*: ",font=sub).grid(row=8,sticky=W,pady=4)
     Entry(student, textvariable=l[7]).grid(row=8,column=1)
 
-    Label(student,text="    Chemistry marks: ",font=sub).grid(row=8,column=2,sticky=W,pady=4)
+    Label(student,text="    Chemistry marks*: ",font=sub).grid(row=8,column=2,sticky=W,pady=4)
     Entry(student, textvariable=l[8]).grid(row=8,column=3)
 
-    Label(student,text="    Maths marks: ",font=sub).grid(row=8,column=4,sticky=W,pady=4)
+    Label(student,text="    Maths marks*: ",font=sub).grid(row=8,column=4,sticky=W,pady=4)
     Entry(student, textvariable=l[9]).grid(row=8,column=5)
 
     #Contact details
@@ -199,7 +199,7 @@ def populate(container):
     contact.grid(row=1,columnspan=7,sticky=W, padx=5, pady=5, ipadx=5, ipady=5)
     
     #Labels
-    Label(contact,text="    Phone no: ",font=sub).grid(row=1,sticky=W,pady=4)
+    Label(contact,text="    Phone no*: ",font=sub).grid(row=1,sticky=W,pady=4)
     Entry(contact, textvariable=l[12]).grid(row=1,column=1)
     
     Label(contact,text="    Email address: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
@@ -209,29 +209,29 @@ def populate(container):
     address_1.grid(row=2,columnspan=6,sticky=W, padx=5, pady=5, ipadx=5, ipady=5)
     #Addrss details
     
-    Label(address_1,text="    Country: ",font=sub).grid(row=1,sticky=W,pady=4)
+    Label(address_1,text="    Country*: ",font=sub).grid(row=1,sticky=W,pady=4)
     enCountry=Entry(address_1, textvariable=l[14])
     enCountry.bind("<KeyRelease-Return>", lambda event, root=address_1, country=l[14], state=l[15]: show_st(event, root, country, state))
     enCountry.grid(row=1,column=1)
     
-    Label(address_1,text="    State: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
+    Label(address_1,text="    State*: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
     
-    Label(address_1,text="    City: ",font=sub).grid(row=2,sticky=W,pady=4)
+    Label(address_1,text="    City*: ",font=sub).grid(row=2,sticky=W,pady=4)
     Entry(address_1, textvariable=l[16]).grid(row=2,column=1)
     
-    Label(address_1,text="    Locality: ",font=sub).grid(row=2,column=2,sticky=W,pady=4)
+    Label(address_1,text="    Locality*: ",font=sub).grid(row=2,column=2,sticky=W,pady=4)
     Entry(address_1, textvariable=l[17]).grid(row=2,column=3)
     
-    Label(address_1,text="    District: ",font=sub).grid(row=2,column=4,sticky=W,pady=4)
+    Label(address_1,text="    District*: ",font=sub).grid(row=2,column=4,sticky=W,pady=4)
     Entry(address_1, textvariable=l[18]).grid(row=2,column=5)
     
-    Label(address_1,text="    Post Office: ",font=sub).grid(row=3,sticky=W,pady=4)
+    Label(address_1,text="    Post Office*: ",font=sub).grid(row=3,sticky=W,pady=4)
     Entry(address_1, textvariable=l[19]).grid(row=3,column=1)
     
-    Label(address_1,text="    Police Station: ",font=sub).grid(row=3,column=2,sticky=W,pady=4)
+    Label(address_1,text="    Police Station*: ",font=sub).grid(row=3,column=2,sticky=W,pady=4)
     Entry(address_1, textvariable=l[20]).grid(row=3,column=3)
     
-    Label(address_1,text="    PINCODE: ",font=sub).grid(row=3,column=4,sticky=W,pady=4)
+    Label(address_1,text="    PINCODE*: ",font=sub).grid(row=3,column=4,sticky=W,pady=4)
     Entry(address_1, textvariable=l[21]).grid(row=3,column=5)
 
 
@@ -271,16 +271,16 @@ def populate(container):
     guardian = LabelFrame(container, text="Guardian details", font=h1)
     guardian.grid(row=2,columnspan=7,sticky=W, padx=5, pady=5, ipadx=5, ipady=5)
     #Labels
-    Label(guardian,text="   Guardian's name: ",font=sub).grid(row=1,sticky=W,pady=4)
+    Label(guardian,text="   Guardian's name*: ",font=sub).grid(row=1,sticky=W,pady=4)
     Entry(guardian, textvariable=l[30]).grid(row=1,column=1)
     
-    Label(guardian,text="   Relation: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
+    Label(guardian,text="   Relation*: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
     Entry(guardian, textvariable=l[31]).grid(row=1,column=3)
     
     Label(guardian,text="   Occupation: ",font=sub).grid(row=2,sticky=W,pady=4)
     Entry(guardian, textvariable=l[32]).grid(row=2,column=1)
     
-    Label(guardian,text="   Phone: ",font=sub).grid(row=3,sticky=W,pady=4)
+    Label(guardian,text="   Phone*: ",font=sub).grid(row=3,sticky=W,pady=4)
     Entry(guardian, textvariable=l[33]).grid(row=3,column=1)
     
     Label(guardian,text="   Email: ",font=sub).grid(row=3,column=2,sticky=W,pady=4)
@@ -289,28 +289,28 @@ def populate(container):
     address_3 = LabelFrame(guardian, text="Address", font=h1)
     address_3.grid(row=4,columnspan=6,sticky=W, padx=5, pady=5, ipadx=5, ipady=5)
     #Addrss details
-    Label(address_3,text="    Country: ",font=sub).grid(row=1,sticky=W,pady=4)
+    Label(address_3,text="    Country*: ",font=sub).grid(row=1,sticky=W,pady=4)
     enCountry=Entry(address_3, textvariable=l[35])
     enCountry.bind("<KeyRelease-Return>", lambda event, root=address_3, country=l[35], state=l[36]: show_st(event, root, country, state))
     enCountry.grid(row=1,column=1)
     
-    Label(address_3,text="    State: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
-    Label(address_3,text="    City: ",font=sub).grid(row=2,sticky=W,pady=4)
+    Label(address_3,text="    State*: ",font=sub).grid(row=1,column=2,sticky=W,pady=4)
+    Label(address_3,text="    City*: ",font=sub).grid(row=2,sticky=W,pady=4)
     Entry(address_3, textvariable=l[37]).grid(row=2,column=1)
     
-    Label(address_3,text="    Locality: ",font=sub).grid(row=2,column=2,sticky=W,pady=4)
+    Label(address_3,text="    Locality*: ",font=sub).grid(row=2,column=2,sticky=W,pady=4)
     Entry(address_3, textvariable=l[38]).grid(row=2,column=3)
     
-    Label(address_3,text="    District: ",font=sub).grid(row=2,column=4,sticky=W,pady=4)
+    Label(address_3,text="    District*: ",font=sub).grid(row=2,column=4,sticky=W,pady=4)
     Entry(address_3, textvariable=l[39]).grid(row=2,column=5)
     
-    Label(address_3,text="    Post Office: ",font=sub).grid(row=3,sticky=W,pady=4)
+    Label(address_3,text="    Post Office*: ",font=sub).grid(row=3,sticky=W,pady=4)
     Entry(address_3, textvariable=l[40]).grid(row=3,column=1)
     
-    Label(address_3,text="    Police Station: ",font=sub).grid(row=3,column=2,sticky=W,pady=4)
+    Label(address_3,text="    Police Station*: ",font=sub).grid(row=3,column=2,sticky=W,pady=4)
     Entry(address_3, textvariable=l[41]).grid(row=3,column=3)
     
-    Label(address_3,text="    PINCODE: ",font=sub).grid(row=3,column=4,sticky=W,pady=4)
+    Label(address_3,text="    PINCODE*: ",font=sub).grid(row=3,column=4,sticky=W,pady=4)
     Entry(address_3, textvariable=l[42]).grid(row=3,column=5)
     Checkbutton(address_3, text="Same as address 1",font=sub).grid(row=4,column=1,sticky=W)
 
